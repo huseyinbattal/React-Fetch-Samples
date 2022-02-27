e2fc52016f9a2b221a70934833b0a4dcaf9b221f
huseyinbatta <huseyinbattal42@gmail.com>
2022-02-27 20:51:37 +0300

    componentDidMount added.

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
+import axios from "axios"
 
 export default class PostList extends Component {
-  render() {
-    return (
-      <div>PostList</div>
-    )
-  }
+    state = {
+        post:null
+    }
+    componentDidMount() {  fetch("https://jsonplaceholder.typicode.com/posts/1")
+    .then((res) => res.json())
+    .then((data) => {
+     
+        this.setState({ users: data, isLoading: false });
+   
+    }); }
+    render() {
+        return (
+            <div>PostList</div>
+        )
+    }
 }
