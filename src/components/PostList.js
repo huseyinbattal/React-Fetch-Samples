b9fd0b2619eb6e2ccbac49ca923a51efef1ed730
huseyinbatta <huseyinbattal42@gmail.com>
2022-02-27 22:28:04 +0300

    axios

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,20 +1,27 @@
-import React, { Component } from 'react'
-import axios from "axios"
+import React, { Component } from "react";
+import axios from "axios";
 
 export default class PostList extends Component {
-    state = {
-        post:null
-    }
-    componentDidMount() {  fetch("https://jsonplaceholder.typicode.com/posts/1")
-    .then((res) => res.json())
-    .then((data) => {
-     
-        this.setState({ users: data, isLoading: false });
-   
-    }); }
-    render() {
-        return (
-            <div>PostList</div>
-        )
-    }
+  state = {
+    post: "",
+  };
+  componentDidMount() {
+    /*fetch("https://jsonplaceholder.typicode.com/posts/1")
+        .then((res) => res.json())
+        .then((data) => {this.setState({ post: data });
+
+        });*/
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts/1")
+      .then((res) => this.setState({post:res.data}));
+  }
+  render() {
+    return (
+      <div>
+        PostList
+        <br />
+        {this.state.post.title}
+      </div>
+    );
+  }
 }
